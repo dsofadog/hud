9da928717c1d4f63e102c823e9742f37f517b216
dsofadog <delaney@so.fa.dog>
2020-12-01 00:40:11 +0200

    Add the call to local

--- lib/hud.rb
+++ lib/hud.rb
@@ -100,7 +100,7 @@ module Hud
           end
 
           puts "getting overides ->  #{screens_dir(overided: true)}/#{symbol}.mote"
-          content = mote("#{screens_dir(overided: true)}/#{symbol}.mote",locals,{})
+          content = mote("#{screens_dir(overided: true)}/#{symbol}.mote",local)
           puts "got overides - ok"
         rescue => exception
           puts "*****************"
