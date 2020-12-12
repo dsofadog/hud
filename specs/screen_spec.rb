b39e5d24df05b15af47b1ae95f03c5ae6db834fa
dsofadog <delaney@so.fa.dog>
2020-12-12 02:08:31 +0200

    Overiden title

--- specs/screen_spec.rb
+++ specs/screen_spec.rb
@@ -22,5 +22,16 @@ describe 'Screen' do
                 .to_html
     expect(content).to eql "# Title\nOveridden Body"
   end 
+
+  it 'to_html with overidden title' do
+    Hud.configure do |config|
+       config.screens_dir = "./specs/screens" 
+    end 
+    class TestScreen < Hud::Screen; end
+    content = TestScreen.new
+                .overide(name: :title, value: "Overidden Title")
+                .to_html
+    expect(content).to eql "Overidden Title\n- Body"
+  end 
   
 end
